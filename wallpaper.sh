fe3d259f0859bb80302546ab8caea91ab49e8ebb
Greg Evseev <cbuctok@gmail.com>
2018-11-02 11:30:16 +0100

    ping & file exists

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 while getopts d: option
 do
- case "${option}"
- in
- d) DIR=$OPTARG;;
- esac
+    case "${option}"
+        in
+        d) DIR=$OPTARG;;
+    esac
 done
+
+## Check if there's network
+function ping_gw() {
+    ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && \
+    return 0 || return 1
+}
+
+ping_gw || (echo "no network, bye" && exit 1)
 
 subreddits=("wallpapers" "art" "iwallpaper")
 selectedsubreddit=${subreddits[$RANDOM % ${#subreddits[@]} ]}
@@ -14,13 +22,16 @@ URL=$(wget -O - http://www.reddit.com/r/$selectedsubreddit.rss | grep -Eo "https
 NAME=$(basename "$URL");
 
 if [ "$DIR" ]
-  then
+then
     FILENAME="$DIR""$NAME";
-  else
+else
     FILENAME=~/Pictures/"$NAME";
 fi
 
-wget "$URL" -O "$FILENAME";
-sleep 1;
+## Check if file exists
+if [ ! -f $FILENAME ]; then
+    wget "$URL" -N -O "$FILENAME";
+    sleep 1;
+fi
 gsettings set org.gnome.desktop.background picture-uri nothing;
 gsettings set org.gnome.desktop.background picture-uri file://"$FILENAME";
